267d077240d7f733493ee38baa927028e56c0660
jujhaar2409 <jujhaar.singh.123@gmail.com>
2020-05-05 13:53:20 +0530

    made the website phone compatible adding touch support and restart button

--- bird.js
+++ bird.js
@@ -1,60 +1,64 @@
 function bird(color) {
-	this.x = 50;
-	this.y = height / 3;
-	this.v = 0;
-	this.a = 0;
-	this.r = 15;
-	this.img1 = loadImage("assets/redbird.png");
-	this.img2 = loadImage("assets/bluebird.png");
-	this.img3 = loadImage("assets/yellowbird.png");
-	this.img = [this.img1, this.img2, this.img3];
-	this.backgroundimg = loadImage("assets/background.png");
-	this.baseimg = loadImage("assets/base.png");
+  this.x = 50;
+  this.y = height / 3;
+  this.v = 0;
+  this.a = 0;
+  this.r = 15;
+  this.img1 = loadImage('assets/redbird.png');
+  this.img2 = loadImage('assets/bluebird.png');
+  this.img3 = loadImage('assets/yellowbird.png');
+  this.img = [this.img1, this.img2, this.img3];
+  this.backgroundimg = loadImage('assets/background.png');
+  this.baseimg = loadImage('assets/base.png');
 
-	if (color == "red") {
-		this.colorChoice = 0;
-	} else if (color == "blue") {
-		this.colorChoice = 1;
-	} else if (color == "yellow") {
-		this.colorChoice = 2;
-	} else {
-		this.colorChoice = floor(random(3));
-	}
-	this.imguse = this.img[this.colorChoice];
+  if (color == 'red') {
+    this.colorChoice = 0;
+  } else if (color == 'blue') {
+    this.colorChoice = 1;
+  } else if (color == 'yellow') {
+    this.colorChoice = 2;
+  } else {
+    this.colorChoice = floor(random(3));
+  }
+  this.imguse = this.img[this.colorChoice];
 
-	this.show = function() {
-		fill(235, 236, 13);
-		image(this.imguse, this.x, this.y);
-	};
+  this.show = function () {
+    fill(235, 236, 13);
+    image(this.imguse, this.x, this.y);
+  };
 
-	this.up = function() {
-		this.v -= 30 * this.a;
-	};
+  this.up = function () {
+    this.v -= 30 * this.a;
+  };
 
-	this.pass = function(other) {
-		if (this.x - this.r > other.x + other.w) {
-			return true;
-		}
-	};
+  this.mobileUp = function () {
+    this.v -= 38 * 0.7;
+  };
 
-	this.update = function() {
-		if (this.y <= (6 * height) / 8) {
-			this.y += this.v;
-			this.v += this.a;
-		} else if (this.y <= (6 * height) / 8) {
-			this.v = 0;
-		}
-	};
+  this.pass = function (other) {
+    if (this.x - this.r > other.x + other.w) {
+      return true;
+    }
+  };
 
-	this.collide = function(o) {
-		if (this.x + this.r / 2 > o.x && this.x - this.r / 2 < o.x + o.w) {
-			if (this.y + this.r / 2 > o.y2 || this.y - this.r / 2 < o.y1) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	};
+  this.update = function () {
+    if (this.y <= (6 * height) / 8) {
+      this.y += this.v;
+      this.v += this.a;
+    } else if (this.y <= (6 * height) / 8) {
+      this.v = 0;
+    }
+  };
+
+  this.collide = function (o) {
+    if (this.x + this.r / 2 > o.x && this.x - this.r / 2 < o.x + o.w) {
+      if (this.y + this.r / 2 > o.y2 || this.y - this.r / 2 < o.y1) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  };
 }
--- sketch.js
+++ sketch.js
@@ -4,113 +4,129 @@ var thePipes = [];
 var myWidth;
 var myHeight;
 
-// function centerCanvas() {
-// 	var x = (windowWidth - width) / 2;
-// 	var y = (windowHeight - height) / 2;
-// 	cnv.position(x, y);
-// }
+let formColor = document.getElementById('color-form');
+formColor.addEventListener('click', color);
+let restartButton = document.getElementById('restart-button');
+restartButton.addEventListener('click', restart);
+let sketchHolder = document.getElementById('sketch-holder');
+sketchHolder.addEventListener('click', onTouch);
 
-formColor = document.getElementById("color-form");
-formColor.addEventListener("click", color);
+function onTouch() {
+  if (theBird.a == 0) {
+    theBird.a = 0.8;
+    theBird.v = 10;
+    theBird.mobileUp();
+  } else {
+    theBird.mobileUp();
+  }
+}
 
-var colorOfBird = "";
+function restart(event) {
+  event.preventDefault();
+  noStroke();
+  cnv = createCanvas(myWidth, myHeight);
+  cnv.parent('sketch-holder');
+  // centerCanvas();
+  theBird = new bird(colorOfBird);
+  s = new score();
+  thePipes = [];
+  base = [];
+  base.push(new Base(0));
+  base.push(new Base(width));
+  loop();
+}
+
+var colorOfBird = '';
 function color(event) {
-	colorOfBird = event.target.value;
-	// console.log(event.target);
-	noStroke();
-	cnv = createCanvas(myWidth, myHeight);
-	cnv.parent("sketch-holder");
-	// centerCanvas();
-	theBird = new bird(colorOfBird);
-	s = new score();
-	thePipes = [];
-	base = [];
-	base.push(new Base(0));
-	base.push(new Base(width));
-	loop();
+  colorOfBird = event.target.value;
+  noStroke();
+  cnv = createCanvas(myWidth, myHeight);
+  cnv.parent('sketch-holder');
+  theBird = new bird(colorOfBird);
+  s = new score();
+  thePipes = [];
+  base = [];
+  base.push(new Base(0));
+  base.push(new Base(width));
+  loop();
 }
 
 function setup() {
-	myHeight = displayHeight * 0.68;
-	myWidth = displayWidth * 0.95;
-	noStroke();
-	cnv = createCanvas(myWidth, myHeight);
-	// centerCanvas();
-	cnv.parent("sketch-holder");
-	theBird = new bird(colorOfBird);
-	s = new score();
-	base.push(new Base(0));
-	base.push(new Base(width));
+  myHeight = displayHeight * 0.68;
+  myWidth = displayWidth * 0.95;
+  noStroke();
+  cnv = createCanvas(myWidth, myHeight);
+  // centerCanvas();
+  cnv.parent('sketch-holder');
+  theBird = new bird(colorOfBird);
+  s = new score();
+  base.push(new Base(0));
+  base.push(new Base(width));
 }
 
 function draw() {
-	image(theBird.backgroundimg, 0, 0, width, height);
-	//image(b.baseimg, 0, 6 * height / 8, width, height / 4);
+  image(theBird.backgroundimg, 0, 0, width, height);
+  //image(b.baseimg, 0, 6 * height / 8, width, height / 4);
 
-	if (frameCount % 70 == 0) {
-		if (theBird.a != 0) {
-			thePipes.push(new pipe());
-		}
-	}
+  if (frameCount % 70 == 0) {
+    if (theBird.a != 0) {
+      thePipes.push(new pipe());
+    }
+  }
 
-	for (var i = 0; i < thePipes.length; i++) {
-		//check whether out of frame
+  for (var i = 0; i < thePipes.length; i++) {
+    //check whether out of frame
 
-		if (thePipes[i].outOfFrame()) {
-			thePipes.shift();
-			s.increase();
-		}
+    if (thePipes[i].outOfFrame()) {
+      thePipes.shift();
+      s.increase();
+    }
 
-		thePipes[i].update();
-		thePipes[i].show();
-		//check collision
+    thePipes[i].update();
+    thePipes[i].show();
+    //check collision
 
-		if (theBird.collide(thePipes[i]) || theBird.y >= (6 * height) / 8) {
-			noLoop();
-		}
-	}
+    if (theBird.collide(thePipes[i]) || theBird.y >= (6 * height) / 8) {
+      noLoop();
+    }
+  }
 
-	for (var j = 0; j < base.length; j++) {
-		if (base[j].outOfFrame()) {
-			base.push(new Base(width * 0.98));
-			base.shift();
-		}
+  for (var j = 0; j < base.length; j++) {
+    if (base[j].outOfFrame()) {
+      base.push(new Base(width * 0.98));
+      base.shift();
+    }
 
-		base[j].update();
-		base[j].show();
-	}
+    base[j].update();
+    base[j].show();
+  }
 
-	theBird.update();
-	theBird.show();
+  theBird.update();
+  theBird.show();
 
-	s.show();
+  s.show();
 }
 
 function keyPressed() {
-	if (key == " ") {
-		if (theBird.a == 0) {
-			theBird.a = 0.7;
-			theBird.v = 10;
-			theBird.up();
-		} else {
-			theBird.up();
-		}
-	} else if (key == "z") {
-		noStroke();
-		cnv = createCanvas(myWidth, myHeight);
-		cnv.parent("sketch-holder");
-		// centerCanvas();
-		theBird = new bird(colorOfBird);
-		s = new score();
-		thePipes = [];
-		base = [];
-		base.push(new Base(0));
-		base.push(new Base(width));
-		loop();
-	}
+  if (key == ' ') {
+    if (theBird.a == 0) {
+      theBird.a = 0.7;
+      theBird.v = 10;
+      theBird.up();
+    } else {
+      theBird.up();
+    }
+  } else if (key == 'z') {
+    noStroke();
+    cnv = createCanvas(myWidth, myHeight);
+    cnv.parent('sketch-holder');
+    // centerCanvas();
+    theBird = new bird(colorOfBird);
+    s = new score();
+    thePipes = [];
+    base = [];
+    base.push(new Base(0));
+    base.push(new Base(width));
+    loop();
+  }
 }
-
-// function windowResized() {
-// 	resizeCanvas(myWidth, myHeight);
-// 	centerCanvas();
-// }
